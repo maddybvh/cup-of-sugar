c7c62de3eb9e54b1c19b123b1cd1d8204106e1aa
maddybvh <maddybvh@gmail.com>
2020-06-23 07:59:52 -0400

    Add bootstrap

--- src/components/SignOut/index.js
+++ src/components/SignOut/index.js
@@ -3,7 +3,11 @@ import React from 'react';
 import { withFirebase } from '../Firebase';
 
 const SignOutButton = ({ firebase }) => (
-  <button type="button" onClick={firebase.doSignOut}>
+  <button
+    type="button"
+    onClick={firebase.doSignOut}
+    className="btn btn-light"
+  >
     Sign Out
   </button>
 );
