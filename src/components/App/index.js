fc0c30f482cee86add36d76e27195306de7dcc8d
maddybvh <maddybvh@gmail.com>
2020-06-23 08:35:21 -0400

    Add container

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -15,11 +15,10 @@ import { withAuthentication } from '../Session';
 
 const App = () => (
   <Router>
-    <div>
+    <div className="container-lg">
       <Navigation />
 
       <hr />
-
       <Route exact path={ROUTES.LANDING} component={LandingPage} />
       <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
       <Route path={ROUTES.SIGN_IN} component={SignInPage} />
--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -3,10 +3,11 @@ import Items from '../Items';
 
 const Landing = () => (
   <div>
-    <h1>Cup of Sugar</h1>
+    <h2>Your neighbors are requesting: </h2>
+    <p>Add requested items here</p>
+    <p>Sign in to request</p>
     <h2>Your neighbors are offering: </h2>
     <Items />
-    <h2>Your neighbors are requesting: </h2>
   </div>
 );
 
