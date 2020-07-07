aac8b8210008d5edd52aedb73ed24fb4d8d58105
maddybvh <maddybvh@gmail.com>
2020-07-07 11:02:32 -0400

    Update container

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -18,7 +18,7 @@ import { withAuthentication } from '../Session';
 const App = () => (
   <Router>
     <Navigation />
-    <div className="container-lg vh-100 mt-4">
+    <div className="container-lg mt-4">
       <Route exact path={ROUTES.LANDING} component={LandingPage} />
       <Route path={ROUTES.SIGN_UP} component={SignUpPage} />
       <Route path={ROUTES.SIGN_IN} component={SignInPage} />
